38ce9ee26bce3a1bbf9785db1a0366a582bc55cf
Baran Babur <baburb@coventry.ac.uk>
2020-11-16 10:54:52 +0000

    remove aside popup after 2 seconds and refactored style.css

--- public/main.js
+++ public/main.js
@@ -1,6 +1,11 @@
-
-/* main.js */
-
 window.addEventListener('DOMContentLoaded', () => {
 	console.log('DOMContentLoaded')
+	// remove popup after 2 seconds
+	const delay = 2000
+	document.querySelector('aside').hidden = false
+	console.log('aside unhidden')
+	window.setTimeout(() => {
+		document.querySelector('aside').hidden = true
+		console.log('aside hidden')
+	}, delay)
 })

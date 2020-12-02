48e3c36da54817f1aa6b6a55472b72e78d78bcbc
Baran Babur <baburb@coventry.ac.uk>
2020-12-02 12:49:06 +0000

    added test for if items can be retrieved

--- unitTests/test-items.js
+++ unitTests/test-items.js
@@ -8,7 +8,7 @@ test('GET ITEMS : test if items are retrieved from database', async test => {
 		await item.addItem('mona lisa', 'http://unsplash.it/500/500', 500, 'for sale', 2)
 		const allItems = await item.getItems()
 		const itemsLength = Object.keys(allItems).length
-		test.is(itemsLength, 6, 'unable to retrieve items')
+		test.is(itemsLength, 6, 'unable to retrieve items') // compare with 6 as there are 6 columns
 	} catch(err) {
 		test.fail(`error thrown ${err}`)
 	} finally {

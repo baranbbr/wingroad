02d3d6694e27772fd3741fafa6599776d10a377e
Baran Babur <baburb@coventry.ac.uk>
2020-12-04 14:32:42 +0000

    uploaded items now have a upload time

--- modules/items.js
+++ modules/items.js
@@ -17,7 +17,7 @@ class Items {
 			// we need this table to store the items of users
 			const sql = 'CREATE TABLE IF NOT EXISTS items\
 				(itemID INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, thumbnail TEXT, price INTEGER, status TEXT,\
-					 userID INTEGER, FOREIGN KEY (userID) REFERENCES users(id));'
+					 userID INTEGER, uploadtime DATETIME DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY (userID) REFERENCES users(id));'
 			await this.db.run(sql)
 			return this
 		})()

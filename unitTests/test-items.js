c392644bab30d3164bcfa8ff3e545e2d99b7c378
Baran Babur <baburb@coventry.ac.uk>
2020-12-10 22:24:47 +0000

    items retrieved newest first and test for it

--- unitTests/test-items.js
+++ unitTests/test-items.js
@@ -34,7 +34,7 @@ test('GET ITEMS 	: test if items retrieved newest first', async test => {
 		await item.addItem('mona lisa 2', 'http://unsplash.it/500/500', '500', 'for sale', 1)
 
 		const allItems = await item.getItems()
-		let valid;
+		let valid
 		allItems[0].uploadtime > allItems[1].uploadtime ? valid = true : valid = false
 		test.is(valid, true, 'items not in correct order') // compare with 6 as there are 6 columns
 	} catch(err) {

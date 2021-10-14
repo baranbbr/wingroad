063869667dd6971ff29964d1e3c59f3db7024ce5
8aran <baranbaburisme@hotmail.com>
2021-10-14 01:33:49 +0100

    still hanging

--- routes/add.js
+++ routes/add.js
@@ -1,6 +1,7 @@
 
 import koaBody from 'koa-body'
 import Router from 'koa-router'
+import fs from 'fs'
 
 const router = new Router()
 // router.use(bodyParser({ formidable: {
@@ -41,24 +42,25 @@ router.get('/add', async ctx => {
  * @name Add Page
  * @route {POST} /add
  */
-router.post('/add', koaBody({multipart: true, uploadDir: '.'}),
+// TODO: keeps hanging on post request
+router.post('/add', koaBody({ multipart: true, uploadDir: '/tmp' }),
 	async ctx => {
-		// const item = await new Items(dbName)
+		const item = await new Items(dbName)
 		try {
-			// const body = ctx.request.body
-			// ctx.hbs.body = ctx.request.body
-			const { path, name, type } = ctx.request.files.thumbnail
+			const { path, name, type } = ctx.request.files.thumbnail;
+			// const { itemName, status, desc, price } = ctx.request.body
+			console.log(ctx.request.body)
 			console.log(`path: ${path}`)
       		console.log(`filename: ${name}`)
       		console.log(`type: ${type}`)
-			// console.log(`name is: ${name}`)
-			// await fs.copy(path, `../avatars/${name}`)
+			// console.log(itemName)
+			await fs.copyFile(path, `public/avatars/${name}`)
 			// await item.userItem(body.name, name,
 			// 	body.description, body.price, ctx.session.userID)
-			return ctx.redirect('/sell?msg=item added')
+			// return ctx.redirect('/sell?msg=item%20added')
 		} catch (err) {
 			console.log(err.message)
-			await ctx.render('error', error.message)
+			await ctx.render('error', err.message)
 		} finally {
 			item.close()
 		}
